04d099619fecdddad80140a93a1f487a87f414e8
Victoria Rydina <victoriarydina@gmail.com>
2023-09-18 15:59:02 +0300

    First commit

---
+++ iosApp/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import shared
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView().ignoresSafeArea(.all)
+    }
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
